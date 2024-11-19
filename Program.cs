80b25e821a5b58b6b7e78ad9f0e1b66bacb5696b
rose <107234238+rose-dangol@users.noreply.github.com>
2024-11-19 21:22:45 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5; int b = 8;
+            Console.WriteLine($"Values before swapping: a={a}, b={b}");
+            a = a + b;     
+            b = a - b;   
+            a = a - b;
+            Console.WriteLine($"Values after swapping: a={a}, b={b}");
+        }
+    }
+}
