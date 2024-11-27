89a7283fadaed9be151e30cae543981d8a6cd5e5
rose <107234238+rose-dangol@users.noreply.github.com>
2024-11-27 13:11:36 +0545

    Add files via upload

---
+++ bubbleSort/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BubbleSortExample
+{
+    class BubbleSort
+    {
+        public void Sort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public void DisplayArray(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ bubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BubbleSortExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[size];
+            
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            BubbleSort sorter = new BubbleSort();
+
+            Console.WriteLine("\nOriginal array:");
+            sorter.DisplayArray(numbers);
+
+            sorter.Sort(numbers);
+
+            Console.WriteLine("\nSorted array:");
+            sorter.DisplayArray(numbers);
+        }
+    }
+}
