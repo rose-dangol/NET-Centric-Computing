b91f4fc3cadc65700c3547c5320c9ab105de3947
rose <107234238+rose-dangol@users.noreply.github.com>
2024-12-07 11:40:05 +0545

    Add files via upload

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -46,31 +46,31 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BackColor = System.Drawing.Color.LightCoral;
+            this.textBox1.BackColor = System.Drawing.Color.MintCream;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.textBox1.Location = new System.Drawing.Point(377, 65);
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(377, 68);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(173, 26);
+            this.textBox1.Size = new System.Drawing.Size(105, 26);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.BackColor = System.Drawing.Color.LightCoral;
+            this.textBox2.BackColor = System.Drawing.Color.MintCream;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.textBox2.Location = new System.Drawing.Point(377, 103);
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(377, 112);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(173, 26);
+            this.textBox2.Size = new System.Drawing.Size(105, 26);
             this.textBox2.TabIndex = 1;
             // 
             // comboBox1
             // 
-            this.comboBox1.BackColor = System.Drawing.Color.LightCoral;
+            this.comboBox1.BackColor = System.Drawing.Color.MintCream;
             this.comboBox1.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.comboBox1.ForeColor = System.Drawing.Color.Black;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.ItemHeight = 29;
             this.comboBox1.Items.AddRange(new object[] {
@@ -78,19 +78,19 @@
             "-",
             "*",
             "/"});
-            this.comboBox1.Location = new System.Drawing.Point(377, 157);
+            this.comboBox1.Location = new System.Drawing.Point(299, 160);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(173, 37);
+            this.comboBox1.Size = new System.Drawing.Size(183, 37);
             this.comboBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.BackColor = System.Drawing.Color.PaleVioletRed;
             this.button1.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button1.Location = new System.Drawing.Point(377, 217);
+            this.button1.Location = new System.Drawing.Point(276, 214);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 64);
+            this.button1.Size = new System.Drawing.Size(206, 53);
             this.button1.TabIndex = 3;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = false;
@@ -100,10 +100,10 @@
             // 
             this.resultLabel.AccessibleDescription = " ";
             this.resultLabel.AutoSize = true;
-            this.resultLabel.BackColor = System.Drawing.Color.LightCoral;
+            this.resultLabel.BackColor = System.Drawing.Color.MintCream;
             this.resultLabel.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resultLabel.ForeColor = System.Drawing.Color.LightGray;
-            this.resultLabel.Location = new System.Drawing.Point(271, 290);
+            this.resultLabel.ForeColor = System.Drawing.Color.SlateGray;
+            this.resultLabel.Location = new System.Drawing.Point(206, 301);
             this.resultLabel.Name = "resultLabel";
             this.resultLabel.Size = new System.Drawing.Size(188, 29);
             this.resultLabel.TabIndex = 4;
@@ -113,10 +113,10 @@
             // operationLabel
             // 
             this.operationLabel.AutoSize = true;
-            this.operationLabel.BackColor = System.Drawing.Color.LightCoral;
+            this.operationLabel.BackColor = System.Drawing.Color.MintCream;
             this.operationLabel.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.operationLabel.ForeColor = System.Drawing.Color.LightGray;
-            this.operationLabel.Location = new System.Drawing.Point(271, 326);
+            this.operationLabel.ForeColor = System.Drawing.Color.SlateGray;
+            this.operationLabel.Location = new System.Drawing.Point(206, 337);
             this.operationLabel.Name = "operationLabel";
             this.operationLabel.Size = new System.Drawing.Size(304, 29);
             this.operationLabel.TabIndex = 5;
@@ -147,7 +147,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(168, 290);
+            this.label3.Location = new System.Drawing.Point(103, 301);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 29);
             this.label3.TabIndex = 8;
@@ -157,7 +157,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(168, 326);
+            this.label4.Location = new System.Drawing.Point(103, 337);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(94, 29);
             this.label4.TabIndex = 9;
@@ -165,10 +165,10 @@
             // 
             // textBox3
             // 
-            this.textBox3.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox3.BackColor = System.Drawing.Color.PowderBlue;
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Font = new System.Drawing.Font("Microsoft YaHei Light", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(239, 14);
+            this.textBox3.Location = new System.Drawing.Point(209, 19);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(382, 31);
             this.textBox3.TabIndex = 10;
@@ -179,7 +179,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.MistyRose;
+            this.BackColor = System.Drawing.Color.PowderBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
